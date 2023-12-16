122d7ab433a0f4f02fad414ddc2b809f13181e4d
mkt989 <sendmoreinfo2000@yahoo.com>
2023-12-15 23:21:21 -0800

    Added 3rd table

--- app/src/main/java/com/example/gymlogfa23/db/AppDatabase.java
+++ app/src/main/java/com/example/gymlogfa23/db/AppDatabase.java
@@ -5,15 +5,17 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 import com.example.gymlogfa23.GymLog;
+import com.example.gymlogfa23.Product;
 import com.example.gymlogfa23.User;
 import com.example.gymlogfa23.db.typeConverters.DateTypeConverter;
 
-@Database(entities = {GymLog.class, User.class}, version = 2)
+@Database(entities = {GymLog.class, User.class, Product.class}, version = 4)
 @TypeConverters(DateTypeConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DB_NAME = "GYMLOG_DATABASE";
     public static final String GYMLOG_TABLE = "GYMLOG_TABLE";
     public static final String USER_TABLE = "USER_TABLE";
+    public static final String PRODUCT_TABLE = "PRODUCT_TABLE";
     public abstract GymLogDAO getGymLogDAO();
 
 }
